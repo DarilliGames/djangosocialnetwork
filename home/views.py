5876582291ab25d84358a80401a396755c11c80c
Stephen Moody <stephengmoody@hotmail.com>
2018-04-08 19:25:50 +0000

    Possbile Home Fix

--- home/views.py
+++ home/views.py
@@ -4,11 +4,7 @@ from games.models import *
 from accounts.models import CharacterProfile
 
 def get_index(request):
-    person1 = get_object_or_404(User, pk=1)
-    person2 = get_object_or_404(User, pk=4)
-    person3 = get_object_or_404(User, pk=3)
-    users = [person1, person2, person3]
-    return render(request, "home/index.html", {"users":users})
+    return render(request, "home/index.html")
     
 def search(request):
     profiles = User.objects.filter(username__icontains=request.GET.get("query"))
